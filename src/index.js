226897476c635d8d20b8dca3b0e42db4adbe1696
shivam <shivamgit@pm.me>
2021-01-02 12:09:12 +0530

    Add mail feature
    Signed-off-by: shivam <shivamgit@pm.me>

--- src/index.js
+++ src/index.js
@@ -1,27 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const nodemailer = require("nodemailer");
+const marked = require("marked");
+const sendMail = require("./mail");
 
 const app = express();
 
-const mailOptions = {
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "4d4546f4381d35",
-    pass: "578018a5625c4f",
-  },
-};
-const transporter = nodemailer.createTransport(mailOptions);
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/submit", (req, res) => {
-  transporter.sendMail({
-    to: "jerryatbusiness@gmail.com",
-    subject: "Form submission recieved",
-    text: JSON.stringify(req.body),
-  });
+  const data = req.body;
+  let s = "|Field|Value|\n|-----|-----|";
+  for (key in data) {
+    s += `\n|${key}|${data[key]}|`;
+  }
+  sendMail(marked(s))
+    .then(() => res.sendStatus(200))
+    .catch((e) => res.status(500).send(e));
 });
 
 app.listen(process.env.PORT || 4000);
