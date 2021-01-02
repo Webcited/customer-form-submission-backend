24acdb8f43c5427b0df278ca21e5ff004d173a32
shivam <shivamgit@pm.me>
2021-01-02 12:11:30 +0530

    Add logging
    Signed-off-by: shivam <shivamgit@pm.me>

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const marked = require("marked");
+const morgan = require("morgan");
 const sendMail = require("./mail");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(morgan("combined"));
 app.post("/submit", (req, res) => {
   const data = req.body;
   let s = "|Field|Value|\n|-----|-----|";
